822c65d815e296052229d337744e35a085faec89
Harshit Yadav <harshityadav172003@gmail.com>
2024-01-05 20:05:42 +0530

    fix: fixed metadata, title, app name

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           alt="DevFlow"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Discuss</span></p>
       </Link>
 
       <GlobalSearch />
@@ -26,7 +26,7 @@ const Navbar = () => {
         <Theme />
 
         <SignedIn>
-          <UserButton 
+          <UserButton
             afterSignOutUrl="/"
             appearance={{
               elements: {
