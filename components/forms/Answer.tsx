18a1dad0ee6218d83dfcd149e20c4d9a94861ab5
Harshit Yadav <harshityadav172003@gmail.com>
2024-01-06 22:47:48 +0530

    fix: removed Ai button

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -45,7 +45,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
       form.reset();
 
-      if(editorRef.current) {
+      if (editorRef.current) {
         const editor = editorRef.current as any;
 
         editor.setContent('');
@@ -58,12 +58,12 @@ const Answer = ({ question, questionId, authorId }: Props) => {
   }
 
   const generateAIAnswer = async () => {
-    if(!authorId) return;
+    if (!authorId) return;
 
     setSetIsSubmittingAI(true);
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`, { 
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`, {
         method: 'POST',
         body: JSON.stringify({ question })
       })
@@ -74,7 +74,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
       const formattedAnswer = aiAnswer.reply.replace(/\n/g, '<br />');
 
-      if(editorRef.current) {
+      if (editorRef.current) {
         const editor = editorRef.current as any;
         editor.setContent(formattedAnswer);
       }
@@ -92,7 +92,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
         <h4 className="paragraph-semibold text-dark400_light800">Write your answer here</h4>
 
-        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+        {/* <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
         onClick={generateAIAnswer}
         >
           {isSubmittingAI ? (
@@ -111,7 +111,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
               Generate AI Answer
               </>
             )}
-        </Button>
+        </Button> */}
       </div>
 
       <Form {...form}>
@@ -125,31 +125,31 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             render={({ field }) => (
               <FormItem className="flex w-full flex-col gap-3">
                 <FormControl className="mt-3.5">
-                <Editor
-                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                  onInit={(evt, editor) => {
-                    // @ts-ignore
-                    editorRef.current = editor
-                  }}
-                  onBlur={field.onBlur}
-                  onEditorChange={(content) => field.onChange(content)}
-                  init={{
-                    height: 350,
-                    menubar: false,
-                    plugins: [
-                      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
-                      'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
-                      'insertdatetime', 'media', 'table'
-                    ],
-                    toolbar: 
-                    'undo redo | ' +
-                    'codesample | bold italic forecolor | alignleft aligncenter |' +
-                    'alignright alignjustify | bullist numlist',
-                    content_style: 'body { font-family:Inter; font-size:16px }',
-                    skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
-                    content_css: mode === 'dark' ? 'dark' : 'light', 
-                  }}
-                />
+                  <Editor
+                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                    onInit={(evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor
+                    }}
+                    onBlur={field.onBlur}
+                    onEditorChange={(content) => field.onChange(content)}
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
+                        'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+                        'insertdatetime', 'media', 'table'
+                      ],
+                      toolbar:
+                        'undo redo | ' +
+                        'codesample | bold italic forecolor | alignleft aligncenter |' +
+                        'alignright alignjustify | bullist numlist',
+                      content_style: 'body { font-family:Inter; font-size:16px }',
+                      skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                      content_css: mode === 'dark' ? 'dark' : 'light',
+                    }}
+                  />
                 </FormControl>
                 <FormMessage className="text-red-500" />
               </FormItem>
@@ -157,13 +157,13 @@ const Answer = ({ question, questionId, authorId }: Props) => {
           />
 
           <div className="flex justify-end">
-              <Button
-                type="submit"
-                className="primary-gradient w-fit text-white"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? 'Submitting...' : 'Submit'}
-              </Button>
+            <Button
+              type="submit"
+              className="primary-gradient w-fit text-white"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'Submitting...' : 'Submit'}
+            </Button>
           </div>
         </form>
       </Form>
