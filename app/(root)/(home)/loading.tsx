854323857ad12d07437cf99fe06c1458ccc3d250
NaturesClub <2105891@kiit.ac.in>
2024-07-24 00:54:44 +0530

    fix: removed ask question and button from Loader

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link"
-
-import { Button } from "@/components/ui/button"
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
     <section>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            Ask a Question
-          </Button>
-        </Link> 
-      </div>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center"></div>
 
       <div className="mb-12 mt-11 flex flex-wrap items-center justify-between gap-5">
         <Skeleton className="h-14 flex-1" />
@@ -36,7 +25,7 @@ const Loading = () => {
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
